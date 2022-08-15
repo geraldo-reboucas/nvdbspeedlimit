576757d611019d7f8a30acb5d5ad3bd28d8c6489
LtGlahn <jensen72@gmail.com>
2022-08-15 12:42:55 +0200

    First working version

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
 # nvdbspeedlimit
 Example code to extract data from NVDB, speed limit data being case in point 
+
+
+# Installation 
+
+Requires the python library [geopandas](https://geopandas.org/en/stable/). A quick and easy way is to use the trusty old  `pip install` in your shell:   
+
+```
+pip install geopandas 
+```
+This is perfect for throwaway environments, such as google cloud platform, or if you don't really use python much at all. 
+
+However, if you're a heavy python user and your python installation has a long life, sucn as your own personal or work computer, we recommend that you go the somewhat more complex route of creating so called `environments` because long term use of `pip install` will at some point create version conflicts. This occurs when the package you want to install today needs a different version of some external 3rd party library than the one you have installed on your system. 
+
+> Example: Geopandas wants to install the package xyzservices version 2022.6.0. Some random library you installed two years ago will not work with version 2022.6.0, but needs the older version that already exists on your system. Solving version conflicts is not for the faint at hearth, which is why creating separate environments is highly recommended. Not only does it prevents version conflicts, it also improves the chance  that your code will be reusable in the future. 
+
+Python has different methods for virtual environments, but the most popular within the data science community is to use the [conda package manager](https://docs.conda.io/en/latest/). And the most popular option for installing conda is to install the [anaconda python distribution](https://www.anaconda.com/products/distribution). 
+
+
+```bash
+conda create -n nvdbspeedlimit -c conda-forge geopandas
+conda activate nvdbspeedlimit
+```
+
+
---
+++ speedlimit2shp.py
@@ -0,0 +1,47 @@
+import nvdbapiv3 
+import geopandas as gpd
+import pandas as pd 
+from shapely import wkt 
+
+if __name__ == '__main__': 
+
+    # Searching for 
+    mySearchObject = nvdbapiv3.nvdbFagdata( 105 )
+
+    # Do you prefer lat/lon coordinates, with the axis order lat - lon (y,x)? 
+    # Specify spatial reference system (srid) epsg:4326 when fetching data from NVDB API 
+    # mySearchObject.filter( {'srid' : 4326})
+
+    # Bounding box for test area 
+    # mySearchObject.filter( {'kartutsnitt' : '11.04189329,58.96015542,11.56670696,59.30649984' } )
+    mySearchObject.filter( {'kartutsnitt' : '283134.63352143,6554956.00500561,293507.29709016,6561801.31869616' } ) 
+
+
+    # Converting to Pandas DataFrame 
+    myDataFrame = pd.DataFrame( mySearchObject.to_records( ))
+
+    # Converting to Geopandas Geodataframe 
+    myDataFrame['geometry'] = myDataFrame['geometri'].apply( wkt.loads )
+    myGDF = gpd.GeoDataFrame( myDataFrame, geometry='geometry', crs=5973 )
+
+    # Reprojecting into lon/lat coordinates (x,y). Note the axis order. 
+    myGDF = myGDF.to_crs( 'epsg:4326' )
+
+    # Attribute naming, translating long Norwegian names into short (10char) English abbreviations
+    # This 10 char limit is one of the curses of the shape file
+    myGDF['SpeedLim'] = myGDF['Fartsgrense'] 
+    myGDF['RoadCat'] = myGDF['vegkategori'] 
+    myGDF['RoadNum'] = myGDF['vegkategori'] + myGDF['vegnummer'].astype(str) 
+
+    # These are linear references to the link sequences of NVDB, so that we can trace problems back to the NVDB network 
+    myGDF['linkID']  = myGDF['veglenkesekvensid']
+    myGDF['linkFrom'] = myGDF['startposisjon']
+    myGDF['linkTo']   = myGDF['sluttposisjon'] 
+
+    # We also include the ID and version number of the speed limit object in NVDB data base
+    columns =  ['SpeedLim', 'RoadCat', 'RoadNum', 'linkID', 'linkFrom', 'linkTo', 'nvdbId', 'versjon', 'geometry']   
+
+    # Writing data to esri shapefile
+    myGDF[columns].to_file( 'nvdbspeedlimit.shp')
+
+    print( list( myGDF.columns ) )
